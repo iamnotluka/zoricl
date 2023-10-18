405d39b9f273f1c4bc8be8f6195bd2debf6a579e
iamnotluka <zoricluk@gmail.com>
2023-10-18 23:53:56 +1000

    done for the 18th

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { PageContents } from './components/PageContents';
 import 'font-awesome/css/font-awesome.min.css';
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,16 +19,20 @@ const darkIcon = (
   );
   
 export const Header: React.FC<HeaderProps> = ({previousPage}) => {
-    const [darkMode, setDarkMode] = useState(true);
-
-    const handleToggle = () => {
-        setDarkMode(!darkMode);
-    };
-
-    useEffect(() => {
-        document.documentElement.setAttribute('data-theme', darkMode ? 'dark' : 'light');
+    const [darkMode, setDarkMode] = useState(() => {
+        const savedTheme = localStorage.getItem('darkMode');
+        return savedTheme === 'true' ? true : false;
+      });
+    
+      const handleToggle = () => {
+        setDarkMode(prevDarkMode => !prevDarkMode);
+      };
+    
+      useEffect(() => {
+        localStorage.setItem('darkMode', darkMode.toString());
+        const theme = darkMode ? 'dark' : 'light';
+        document.documentElement.setAttribute('data-theme', theme);
       }, [darkMode]);
-
     return (
         <div className="header">
             {previousPage != 'none' && <Link to={previousPage}><p><i className='fa fa-angle-left fa-2x'></i> Back</p></Link>}
