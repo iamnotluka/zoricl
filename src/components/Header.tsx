012eafa9e91e498bd357aeef68fa30bf038becb7
iamnotluka <zoricluk@gmail.com>
2023-10-20 21:16:04 +1000

    adding the copy link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,27 +22,54 @@ export const Header: React.FC<HeaderProps> = ({previousPage}) => {
     const [darkMode, setDarkMode] = useState(() => {
         const savedTheme = localStorage.getItem('darkMode');
         return savedTheme === 'true' ? true : false;
-      });
+    });
     
-      const handleToggle = () => {
-        setDarkMode(prevDarkMode => !prevDarkMode);
-      };
+    const handleToggle = () => {
+      setDarkMode(prevDarkMode => !prevDarkMode);
+    };
+
+    const [copied, setCopied] = useState(() => false)
     
       useEffect(() => {
         localStorage.setItem('darkMode', darkMode.toString());
         const theme = darkMode ? 'dark' : 'light';
         document.documentElement.setAttribute('data-theme', theme);
       }, [darkMode]);
+
+      const handleShareClick = async (e: any) => {
+      e.preventDefault(); // Prevent default behavior of <a> tag
+      
+      const url = window.location.href;
+
+      const textarea = document.querySelector('.my-textarea') as HTMLTextAreaElement;;
+      textarea.readOnly = true; // This prevents the keyboard from popping up
+      textarea.value = url;
+      textarea.select();
+      const success = document.execCommand('copy');
+
+      if (success) {
+        setCopied(true);
+        // Show an alert to the user
+        setTimeout(() => {
+          setCopied(false);
+        }, 1000) 
+      } else {
+      }
+
+      };
+
     return (
         <div className="header">
             {previousPage != 'none' && <Link to={previousPage}><p><i className='fa fa-angle-left fa-2x'></i> Back</p></Link>}
             {previousPage == 'none' && <div className='disabled-back'><p><i className='fa fa-angle-left fa-2x'></i> Back</p></div>}
-            <Toggle
+            {<a href='#' onClick={handleShareClick}><strong>{!copied ? 'Copy URL' : 'Copied!'}</strong> <i className='fa fa-share'></i></a>}
+            {/* <Toggle
             checked={darkMode}
             onChange={handleToggle}
             icons={false}
             // icons={{ checked: darkIcon, unchecked: lightIcon }}
-            aria-label='Dark mode toggle'/>
+            aria-label='Dark mode toggle'/> */}
+            <textarea className="my-textarea"></textarea>
         </div>
     )
 }
