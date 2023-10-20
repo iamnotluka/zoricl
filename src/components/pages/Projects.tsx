365359f9f37cc32cb35dd43787060403a15c5bf8
iamnotluka <zoricluk@gmail.com>
2023-10-20 23:07:14 +1000

    finalising career

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Header } from '../Header';
+import { MarkdownSection } from '../MarkdownSection';
+import DateSignature from '../DateSignature';
 
 interface ProjectsProps {
     backPage: string;
@@ -7,7 +10,9 @@ interface ProjectsProps {
 const Projects: React.FC<ProjectsProps> = ({backPage}) => {
   return (
     <div>
-      
+      <Header previousPage={backPage}></Header>
+      <DateSignature date="18 October 2023 at 10:15am"/>
+      <MarkdownSection markdownFileName='projects.txt'/>
     </div>
   )
 }
