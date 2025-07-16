429e808f633937b92b46dea54bff793232dbc35f
iamnotluka <zoricluk@gmail.com>
2025-07-16 20:33:47 +1000

    Cleaning some stuff

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,4 +1,3 @@
-import { MarkdownSection } from "../../MarkdownSection";
 import DateSignature from "../../DateSignature";
 import { Link } from "react-router-dom";
 import { Header } from "../../Header";
@@ -8,46 +7,60 @@ export default function Home() {
 		<div>
 			<Header previousPage="none" />
 			<DateSignature date="18 October 2023 at 8:36pm" />
-			<h4>Hi there! 👋</h4>
-			<p>I'm Luka, a software engineer and marketer.</p>
-			<img src="/profile_image.jpg" alt="Profile Image" />
-			<MarkdownSection markdownFileName="introduction.txt" />
+			<h4>Luka Zoric</h4>
+			<p>Hi there, I'm Luka! 👋</p>
+			<p>
+				I build systems, do sales and marketing, read a whole bunch, think hard and maintain a sub 45 resting heart
+				rate.
+			</p>
+			{/* <img src="/profile_image.jpg" alt="Profile Image" /> */}
+			<p>
+				I've been a software engineer for about 5 years now, spending most of my time working at Amazon's grocery and
+				compliance sectors.
+			</p>
+			<p>
+				I am currently contracting out my hours part time while learning about business and building my own products.
+			</p>
 			<p>
 				Add me on{" "}
-				<a target="_blank" href="https://www.linkedin.com/in/luka-zoric/">
+				<a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/luka-zoric/">
 					LinkedIn
 				</a>{" "}
 				or{" "}
-				<a target="_blank" href="https://www.instagram.com/zoricl_/">
+				<a target="_blank" rel="noreferrer" href="https://www.instagram.com/zoricl_/">
 					Instagram
 				</a>{" "}
 				to connect, or send an email to <a href="mailto:luka@zoricl.io">luka@zoricl.io.</a>
 			</p>
 			<p>
-				If you're a recruiter or just interested what I did in past couple of years - read my{" "}
-				<Link to="/career">career summary</Link>.
-			</p>
-			<h4>💻 Software Projects</h4>
-			<p>
-				Feel free to check out my{" "}
-				<a target="_blank" href="https://github.com/iamnotluka">
-					GitHub
-				</a>{" "}
-				and deep dive my code. Any feedback or question will be highly appreciated!
-			</p>
-			<h5>Current Project: Interpreter in Go</h5>
-			<p className="subsection-date-li">Updated: 15th April 2024</p>
-			<p>
-				I am currently implementing coding language interpreter in Go, to learn Go. Ideally, I'd like to be able to get
-				to the point where I can solve leetcode questions in the custom language.
-			</p>
-			<p>
-				Link to{" "}
-				<a target="_blank" href="https://github.com/iamnotluka/interpreter-in-go">
-					GitHub project
+				I am also on{" "}
+				<a target="_blank" rel="noreferrer" href="https://x.com/zoric_luka_">
+					X
 				</a>
 				.
 			</p>
+			<h4>💻 Projects</h4>
+			<p>
+				Feel free to check out my{" "}
+				<a target="_blank" rel="noreferrer" href="https://github.com/iamnotluka">
+					GitHub
+				</a>{" "}
+				, but majority of my code is private...
+			</p>
+			<h5>InterviewGuru.io</h5>
+			<p className="subsection-date-li">Launch Date: 31st July 2025</p>
+			<p>Basically Leetcode with AI + mock interviews.</p>
+			Link:{" "}
+			<a href="https://www.interviewguru.io" rel="noreferrer" target="_blank">
+				www.interviewguru.io
+			</a>
+			<h5>MVMNT MTHD</h5>
+			<p className="subsection-date-li">Launch Date: 31st July 2025</p>
+			<p>We sell fitness programs for people who want to take their training to the next level.</p>
+			Link:{" "}
+			<a href="https://www.mvmntmthd.com" rel="noreferrer" target="_blank">
+				www.mvmntmthd.com
+			</a>
 			{/* <p>
 				Check it out <Link to="/software-resources">here.</Link>
 			</p>*/}
